f49c0dddfc640eef7d19ba3dea1e3c48e18653e6
izaack55182 <L20TE0298@TEPEACA.TECNM.MX>
2026-05-22 14:55:09 -0600

    feat: add Cloudflare Pages server-side adapter and update deploy command to support build artifacts

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -2,5 +2,16 @@ import handle from 'hono-react-router-adapter/cloudflare-pages'
 // @ts-ignore - build will be present after react-router build
 import * as build from '../build/server'
 import server from '../server'
+import { getLoadContext } from '../server'
 
-export const onRequest = handle(build, server)
+export const onRequest = handle(build, server, {
+    getLoadContext: (event: any) => {
+        // En Cloudflare Pages, las variables de entorno reales están en event.env
+        const c = event.context.hono.context
+
+        // Nos aseguramos de que el env de Cloudflare esté en el contexto de Hono
+        c.env = Object.assign({}, c.env, event.env)
+
+        return getLoadContext(c)
+    }
+})
